8f0ea2429cd8cedb416e69019ae7327d302ab962
Vadim Gordeev <enzogord@gmail.com>
2026-03-20 15:32:04 +0300

    RdlCri переведен в netstandard2.0

--- RdlCri/Properties/AssemblyInfo.cs
+++ RdlCri/Properties/AssemblyInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -30,8 +28,7 @@ using System.Security;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("4.15.*")]
+[assembly: AssemblyVersion("4.15.0.0")]
+[assembly: AssemblyFileVersion("4.15.0.0")]
 
 
